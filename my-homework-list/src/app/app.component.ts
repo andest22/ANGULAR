280c5a1e6ddc98e01576afb194b09d11b2030f53
andest22 <andresecasvar05@gmail.com>
2024-11-05 15:49:59 -0500

    actualizacion 5/11/2024

--- my-homework-list/src/app/app.component.ts
+++ my-homework-list/src/app/app.component.ts
@@ -1,13 +1,28 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {HomeComponent} from './home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
+  imports: [HomeComponent],
+  template: `
+  <main>
+    <div class="container">
+      <nav class="navbar navbar-light bg-light">
+      <div class="container">
+        <a class="navbar-brand" href="#">
+          <img src="/assets/img/icons/home.svg" alt="" width="30" height="24">
+        </a>
+      </div>
+    </nav>
+      <section class="container">
+        <app-home></app-home>
+      </section>
+      </div>
+    </main>`,
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'my-homework-list';
+  title = 'my-app-angular';
 }
